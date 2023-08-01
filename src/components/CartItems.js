eaa884601c4c85293efb0874e85269789a96fa01
Nivs <nivsoficial@gmail.com>
2023-08-01 18:01:09 -0300

    att about cart [WIP]

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,14 +1,26 @@
 import React from "react";
 import CartItem from "./CartItem";
 import "./Cart.css";
+import { useSelector } from "react-redux";
 const CartItems = () => {
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  console.log(cartItems)
   return (
     <div className="cart-container">
       <h2>Your Cart</h2>
-      <ul>
-        <li>
-          <CartItem id={1} price={2500} name={"Macbook"} />
-        </li>
+        <ul>
+          { cartItems.map((item, k) => {
+            return <li key={k}>
+              {" "}
+              <CartItem 
+                id={item.id} 
+                price={item.price} 
+                name={item.name} 
+                total={item.totalPrice}
+                />
+              {" "}
+              </li>
+          })}
       </ul>
     </div>
   );
